d16df090f1072f47e66763b04d6fcf3d86d11f84
sunilkdevops <87353894+sunilkdevops@users.noreply.github.com>
2021-07-18 09:51:14 +0530

    Update big3.c
    webhook

--- big3.c
+++ big3.c
@@ -6,7 +6,7 @@ void biggest3() {
     
     // if n1 is greater than both n2 and n3, n1 is the largest
     if (n1 >= n2 && n1 >= n3)
-        printf("%.2f is the largest number.", n1);
+        printf("%.2f is the largest numr.", n1);
 
     // if n2 is greater than both n1 and n3, n2 is the largest
     if (n2 >= n1 && n2 >= n3)
